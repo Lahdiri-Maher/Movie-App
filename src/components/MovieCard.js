b74f2ee29b8c4ce0cf3f989c59286a0df50c5e06
maher <lahdiri.maher@gmail.com>
2022-05-10 15:47:38 +0100

    first commit

---
+++ src/components/MovieCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const MovieCard = ({ movies }) => {
+    return (
+        <div className="movie">
+          
+        </div>
+      );
+    };
+    
+    
+    export default MovieCard;
